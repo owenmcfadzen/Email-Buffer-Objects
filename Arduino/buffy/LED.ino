2b21523dff63fa39b1cb7ad41c591b89e7ce989b
Luke Sturgeon <lukesturgeon@Lukes-MacBook-Pro.local>
2013-06-28 10:01:56 +0200

    Merge branch 'master' of https://github.com/unavoidableart/Email-Buffer-Objects
    Conflicts:
    	Arduino/buffy/LED.ino
    	Arduino/buffy/buffy.ino

--- Arduino/buffy/LED.ino
+++ Arduino/buffy/LED.ino
@@ -9,10 +9,38 @@ int led_sequence[9] = {
   B11111110,
   B11111111}; 
  
+int oldLEDVal = 0;
+int led_max = 4095; //WARNING, very high
+int led_high = 2000;
+int led_med_high = 1500;
+int led_med = 1000;
+int led_low = 500;
+int led_off = 0;
 
 void set_multiple_leds(int n){
+  Serial.print("turn on ");
+  Serial.print(n);
+  Serial.println(" lights");
+  
   n = constrain(n, 0, 8);
   digitalWrite(LED_LATCH_PIN, LOW);
   shiftOut(LED_DATA_PIN, LED_CLOCK_PIN, MSBFIRST, led_sequence[n]);
   digitalWrite(LED_LATCH_PIN, HIGH);
+=======
+void setLed(int n){
+  
+  n = constrain(n, 0, 15);
+  int d = 0;
+    //Fade out
+    }
+  }
+  else if(oldLEDVal < n) {
+    for (int i = oldLEDVal; i < n; i++){
+      //Fade in
+      tlc_addFade(i, led_off, led_high, time+d, time+200+d);
+      d += 80;
+    }
+  }
+  oldLEDVal = n;
+>>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
 }
--- Arduino/buffy/buffy.ino
+++ Arduino/buffy/buffy.ino
@@ -1,24 +1,45 @@
-#define DEVICE_ID 'B'
+#define DEVICE_ID 'A'
 #define FORCE_SENSOR_PIN A0 
+<<<<<<< HEAD
 #define LED_DATA_PIN 2
 #define LED_LATCH_PIN 3
 #define LED_CLOCK_PIN 4
+#define MOTOR_PIN 7
 
 void setup(){
   Serial.begin(9600);
+  
+  pinMode(MOTOR_PIN, OUTPUT);
+  digitalWrite(MOTOR_PIN, LOW);
+  
   pinMode(LED_DATA_PIN, OUTPUT);
   pinMode(LED_LATCH_PIN, OUTPUT);
   pinMode(LED_CLOCK_PIN, OUTPUT);
+=======
+
+/*LED Libraries*/
+#include "Tlc5940.h"
+#include "tlc_fades.h"
+TLC_CHANNEL_TYPE channel;
+
+
+void setup(){
+  Serial.begin(9600);
+  /*LED Library*/
+  Tlc.init();
+>>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
 }
 
 void loop(){
   update_sensor();
+  tlc_updateFades(millis());
 }
 
 /* Events */
 void serial_event(byte serial_value) {
   /* Assume that serial_value is an int and turn on LEDs */
-  set_multiple_leds(serial_value);
+//  set_multiple_leds(serial_value);
+  setLed(serial_value);
 }
 
 void lift_event() {
