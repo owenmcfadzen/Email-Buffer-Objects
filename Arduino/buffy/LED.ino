494cc1a9b57448ad416edc1ecde467150c09f3d2
Luke Sturgeon <lukesturgeon@Lukes-MacBook-Pro.local>
2013-06-28 10:49:47 +0200

    Fixed broken code + LED numbers
    A number from serial wasn't turning on all the LEDs, I changed the for
    loop to fix this

--- Arduino/buffy/LED.ino
+++ Arduino/buffy/LED.ino
@@ -1,50 +1,37 @@
-int oldLEDVal = 0;
-int led_max = 4095; //WARNING, very high
-int led_high = 2000;
-int led_med_high = 1500;
-int led_med = 1000;
-int led_low = 500;
-int led_off = 0;
+#define LED_MAX 4095
+#define LED_HIGH 1000
+#define LED_OFF 0
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-void set_multiple_leds(int n){
-  Serial.print("turn on ");
-  Serial.print(n);
-  Serial.println(" lights");
+int led_old_val = 0;
+
+void setLed(int led_new_val){
+  /* Accept a number: 0 = no leds, 1-8 = leds */
+  led_new_val = constrain(led_new_val, 0, 8);
   
-  n = constrain(n, 0, 8);
-  digitalWrite(LED_LATCH_PIN, LOW);
-  shiftOut(LED_DATA_PIN, LED_CLOCK_PIN, MSBFIRST, led_sequence[n]);
-  digitalWrite(LED_LATCH_PIN, HIGH);
-=======
-=======
->>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
-void setLed(int n){
+  Serial.print("turning on ");
+  Serial.print(led_new_val);
+  Serial.println(" LEDs");
   
-  n = constrain(n, 0, 15);
   int time = millis();
-  int d = 0;
+  int led_delay = 0;
   
-  if(oldLEDVal > n){
+  if(led_old_val > led_new_val){
     //Fade out
-    for(int i = oldLEDVal; i > n; i--){
-      tlc_addFade(i, led_high, led_off, time+d, time+200+d);
-      d += 80; 
+    for(int i = led_old_val; i > led_new_val; i--){
+      tlc_addFade(i, LED_HIGH, LED_OFF, time + led_delay, time + led_delay + 200);
+      led_delay += 80; 
     }
   }
-  else if(oldLEDVal < n) {
-    d = 0;
-    for (int i = oldLEDVal; i < n; i++){
+  else if(led_old_val < led_new_val) {
+    led_delay = 0;
+    for (int i = led_old_val; i <= led_new_val; i++){
       //Fade in
-      tlc_addFade(i, led_off, led_high, time+d, time+200+d);
-      d += 80;
+      tlc_addFade(i, LED_OFF, LED_HIGH, time + led_delay, time + led_delay + 200);
+      led_delay += 80;
     }
   }
-  oldLEDVal = n;
-<<<<<<< HEAD
->>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
-=======
->>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
+  
+  /* Remember the current value for next time */
+  led_old_val = led_new_val;
 }
 
--- Arduino/buffy/buffy.ino
+++ Arduino/buffy/buffy.ino
@@ -1,47 +1,23 @@
 #define DEVICE_ID 'A'
-#define FORCE_SENSOR_PIN A0 
-<<<<<<< HEAD
-<<<<<<< HEAD
+#define FORCE_SENSOR_PIN A0
 #define LED_DATA_PIN 2
 #define LED_LATCH_PIN 3
 #define LED_CLOCK_PIN 4
 #define MOTOR_PIN 7
 
+/*LED Libraries*/
+#include "Tlc5940.h"
+#include "tlc_fades.h"
+
 void setup(){
   Serial.begin(9600);
   
+  /* Initialise the solenoid pins */
   pinMode(MOTOR_PIN, OUTPUT);
   digitalWrite(MOTOR_PIN, LOW);
   
-  pinMode(LED_DATA_PIN, OUTPUT);
-  pinMode(LED_LATCH_PIN, OUTPUT);
-  pinMode(LED_CLOCK_PIN, OUTPUT);
-=======
-
-/*LED Libraries*/
-#include "Tlc5940.h"
-#include "tlc_fades.h"
-TLC_CHANNEL_TYPE channel;
-
-
-void setup(){
-  Serial.begin(9600);
-  /*LED Library*/
+  /* LED Library */
   Tlc.init();
->>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
-=======
-
-/*LED Libraries*/
-#include "Tlc5940.h"
-#include "tlc_fades.h"
-TLC_CHANNEL_TYPE channel;
-
-
-void setup(){
-  Serial.begin(9600);
-  /*LED Library*/
-  Tlc.init();
->>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
 }
 
 void loop(){
@@ -52,7 +28,6 @@ void loop(){
 /* Events */
 void serial_event(byte serial_value) {
   /* Assume that serial_value is an int and turn on LEDs */
-//  set_multiple_leds(serial_value);
   setLed(serial_value);
 }
 
@@ -71,9 +46,3 @@ void short_press_event() {
 void long_press_event() {
   Serial.println("Long Press");
 }
-
-
-
-
-
-
