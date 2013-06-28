842e94529c130285fa5f26d9728176e48432c37a
Luke Sturgeon <lukesturgeon@Lukes-MacBook-Pro.local>
2013-06-28 10:02:35 +0200

    trying to resolve conflicts in code

--- Arduino/buffy/LED.ino
+++ Arduino/buffy/LED.ino
@@ -1,14 +1,3 @@
-int led_sequence[9] = {
-  B00000000,
-  B10000000,
-  B11000000,
-  B11100000,
-  B11110000,
-  B11111000,
-  B11111100,
-  B11111110,
-  B11111111}; 
- 
 int oldLEDVal = 0;
 int led_max = 4095; //WARNING, very high
 int led_high = 2000;
@@ -17,6 +6,7 @@ int led_med = 1000;
 int led_low = 500;
 int led_off = 0;
 
+<<<<<<< HEAD
 void set_multiple_leds(int n){
   Serial.print("turn on ");
   Serial.print(n);
@@ -30,12 +20,18 @@ void set_multiple_leds(int n){
 void setLed(int n){
   
   n = constrain(n, 0, 15);
+  int time = millis();
   int d = 0;
+  
   if(oldLEDVal > n){
     //Fade out
+    for(int i = oldLEDVal; i > n; i--){
+      tlc_addFade(i, led_high, led_off, time+d, time+200+d);
+      d += 80; 
     }
   }
   else if(oldLEDVal < n) {
+    d = 0;
     for (int i = oldLEDVal; i < n; i++){
       //Fade in
       tlc_addFade(i, led_off, led_high, time+d, time+200+d);
@@ -45,3 +41,4 @@ void setLed(int n){
   oldLEDVal = n;
 >>>>>>> c163f1b17c6448722bdcfc82a11472e133446aa2
 }
+
